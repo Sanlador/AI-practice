ab1f55b33a1a14244ccdc56cc9e8e904fa3e058c
Richard Cunard <cunardr@oregonstate.edu>
2019-02-06 15:39:38 -0800

    Added RBFS
    Added function RBFS solver function.

--- puzzle.py
+++ puzzle.py
@@ -61,10 +61,10 @@ def scramble(board, m):
 			r = random.randint(0,3)
 			if move(board, compass[r]) != []:
 				board = move(board, compass[r])
-				control = True	
+				control = True
 				#print(board)
 	return board
-							
+
 #measures a manhattan heuristic	of a given board position
 def manhattan(board):
 	h = 0
@@ -92,17 +92,17 @@ def IDA(root):
 		bound = t
 	return t
 
-#performs a recurseive search using a manhatten heuristic
+#performs a recurseive search using a manhattan heuristic
 def IDASearch(path, g, bound):
 	root = path[-1]
 	f = g + manhattan(root)
-	
+
 	#print(f)
 	if f > bound:
 		return f
 	if goalTest(root):
 		return "found"
-	
+
 	#selects legal choices
 	choice = []
 	min = math.inf
@@ -117,7 +117,7 @@ def IDASearch(path, g, bound):
 			if np.array_equal(j,path[j]):
 				control = True
 		if control == False:
-			
+
 			#print(root)
 			path.append(i)
 			t = IDASearch(path, g + 1, bound)
@@ -127,9 +127,45 @@ def IDASearch(path, g, bound):
 				min = t
 			del path[-1]
 	return min
-	
+
+#performs a recursive best first search
+#3114
+def RBFS(board, fLimit):
+	fn = manhattan(board)
+	if goalTest(board):
+		return "success", board
+	choice = []
+	for i in range(4):
+		if move(board, compass[i]) != []:
+			moveCost = manhattan((move(board, compass[i])))
+			choice.append((move(board, compass[i]), max(moveCost,fn)))
+
+	succ = [choice[0]]
+	for i in range(1, len(choice)):
+		#print(i)
+		for j in range(len(succ)):
+			if manhattan(choice[i][0]) < manhattan(succ[j][0]):
+				succ.insert(j,choice[i])
+			elif j == len(succ) - 1:
+				succ.append(choice[i])
+
+	control = True
+	while control == True:
+		#print(succ)
+		best = succ[0][0]
+		#print(best)
+		alt = succ[1][0]
+		if manhattan(best) > fLimit:
+			return "Fail", best
+		result, best = RBFS(best, min(fLimit, manhattan(alt)))
+		if result == "success":
+			return result, best
+			control = False
+
 
 
 b1 = createBoard()
-b1 = scramble(b1, 50)
-print(IDA(b1))
+b1 = scramble(b1, 20)
+b2 = b1
+#print(IDA(b1))
+print(RBFS(b2, 50))
