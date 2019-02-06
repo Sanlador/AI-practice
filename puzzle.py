b2dbb4cb41a751959a9880dcbf7f03a0ec9f7b4c
Sanlador <cunardr@oregonstate.edu>
2019-02-05 19:53:14 -0800

    Implemented A* heuristic and IDA* search
    After refactoring code to account for the needs of the search, I was
    able to implement a working IDA* search algorithm able to find the
    answer even when scrambling with 100 moves.

--- puzzle.py
+++ puzzle.py
@@ -1,76 +1,70 @@
 #This program implements the RDFS and IDA* to solve a 15 puzzle
 import random
+import math
 import numpy as np
 
-class game:
-	position = [3,3]
-	board = np.zeros((4,4))
-	compass = ["up","down","left","right"]
-	direction = {
-		"up": (-1, 0),
-		"down": (1, 0),
-		"right": (0, 1),
-		"left": (0, -1)
-	}
+
+compass = ["up","down","left","right"]
+direction = {
+	"up": (-1, 0),
+	"down": (1, 0),
+	"right": (0, 1),
+	"left": (0, -1)
+}
 
 	#create a 4x4 grid with values 0 to 15 randomly assigned (0 is considered the empty space)
-	def __init__(self):
-		count = 1
-		for i in range(4):
-			for j in range(4):
-				self.board[i][j] = count
-				count += 1
-		self.board[3][3] = 0
+def createBoard():
+	board = np.zeros((4,4))
+	count = 1
+	for i in range(4):
+		for j in range(4):
+			board[i][j] = count
+			count += 1
+	board[3][3] = 0
+	return board
 
-	#returns a board position for a given a move
-	def move(self, direction):
-		board = np.copy(self.board)
-		position = np.copy(self.position)
+#returns a board position for a given a move
+def move(board, dir):
+	boardCopy = np.copy(board)
+	position = np.where(board == 0)
+	#check for illegal moves
+	if position[0][0] == 0 and dir == "up":
+		return []
+	if position[0][0] == 3 and dir == "down":
+		return []
+	if position[1][0] == 0 and dir == "left":
+		return []
+	if position[1][0] == 3 and dir == "right":
+		return []
+	move = [position[0][0] + direction[dir][0], position[1][0] + direction[dir][1]]
+	boardCopy[position[0][0]][position[1][0]] = boardCopy[move[0]][move[1]]
+	boardCopy[move[0]][move[1]] = 0
+	return boardCopy
 
-		#check for illegal moves
-		if position[0] == 0 and direction == "up":
-			return []
-		if position[0] == 3 and direction == "down":
-			return []
-		if position[1] == 0 and direction == "left":
-			return []
-		if position[1] == 3 and direction == "right":
-			return []
+#checks for sucessful board state
+def goalTest(board):
+	count = 1
+	for i in range(4):
+		for j in range(4):
+			if i == 3 and j == 3:
+				return True
+			elif board[i][j] != count:
+				return False
+			count += 1
 
-		move = [position[0] + self.direction[direction][0], position[1] + self.direction[direction][1]]
-		board[position[0]][position[1]] = board[move[0]][move[1]]
-		board[move[0]][move[1]] = 0
-		return board, move
-
-	#executes a move on the board
-	def performMove(self, direction):
-		if self.move(direction) != []:
-			self.board, self.position = self.move(direction)
-			#print(self.board)
-			return True
-		else:
-			return False
-
-	#checks for sucessful board state
-	def goalTest(self):
-		count = 1
-		for i in range(4):
-			for j in range(4):
-				if i == 3 and j == 3:
-					return True
-				elif self.board[i][j] != count:
-					return False
-				count += 1
-
-	#Performs a series of m random moves where m is an inputted integer value
-	def scramble(self, m):
-		compass = ["up", "down", "left", "right"]
-		for i in range(m):
-			control = False
-			while control == False:
-				if self.performMove(compass[random.randint(0,3)]):
-					control = True
-
+#Performs a series of m random moves where m is an inputted integer value
+def scramble(board, m):
+	compass = ["up", "down", "left", "right"]
+	for i in range(m):
+		control = False
+		while control == False:
+			r = random.randint(0,3)
+			if move(board, compass[r]) != []:
+				board = move(board, compass[r])
+				control = True	
+				#print(board)
+	return board
+							
 #measures a manhattan heuristic	of a given board position
 def manhattan(board):
 	h = 0
@@ -86,22 +80,56 @@ def manhattan(board):
 		h += abs(loc[0] - row) + abs(loc[1] - col)
 	return h
 
-def IDA(game, path = [], g = 0):
-	if game.goalTest():
-		return path, len(path)
+#initiates an IDA* search
+def IDA(root):
+	bound = manhattan(root)
+	path = [root]
+	t = ""
+	while t != "found":
+		#begins a recursive search
+		t = IDASearch(path, 0, bound)
+		#increases the bound, allowing the search to go deeper
+		bound = t
+	return t
+
+#performs a recurseive search using a manhatten heuristic
+def IDASearch(path, g, bound):
+	root = path[-1]
+	f = g + manhattan(root)
+	
+	#print(f)
+	if f > bound:
+		return f
+	if goalTest(root):
+		return "found"
+	
+	#selects legal choices
 	choice = []
+	min = math.inf
 	for i in range(4):
-		if game.move(game.compass[i]) != []:
-			choice.append((game.move(game.compass[i]), game.compass[i])
-	minCost = choice[0]
-	for i in range(1,len(choice)):
-		if manhattan(i) < manhattan(minCost):
-			minCost = choice[i]
-	if manhattan(minCost) + 1 + g < 
+		if move(root, compass[i]) != []:
+			choice.append(move(root, compass[i]))
+	#print(choice)
+	for i in choice:
+		control = False
+		#checks if configuration has a previously reached state, and culls it from the list of options if so
+		for j in range(len(path)):
+			if np.array_equal(j,path[j]):
+				control = True
+		if control == False:
+			
+			#print(root)
+			path.append(i)
+			t = IDASearch(path, g + 1, bound)
+			if t == "found":
+				return "found"
+			elif t < min:
+				min = t
+			del path[-1]
+	return min
+	
 
 
-
-g1 = game()
-
-g1.scramble(10)
-IDA(g1)
+b1 = createBoard()
+b1 = scramble(b1, 50)
+print(IDA(b1))
