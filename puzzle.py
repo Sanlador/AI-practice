3862320e8c96a513099b71d388c8c55cd7f6c6c3
Sanlador <cunardr@oregonstate.edu>
2019-02-17 20:41:20 -0800

    Began sudoku
    Implemented sudoku puzzle along with alldiff constraint, which
    determines available domain for the sudoku puzzle framed as a CSP. Also
    implemented both naked and hidden singles inferences.

--- puzzle.py
+++ puzzle.py
@@ -1,8 +1,10 @@
 #This program implements the RDFS and IDA* to solve a 15 puzzle
 import random
 import math
+import time
 import numpy as np
-
+globalA = 0
+globalB = 0
 
 compass = ["up","down","left","right"]
 direction = {
@@ -65,8 +67,8 @@ def scramble(board, m):
 				#print(board)
 	return board
 
-#measures a manhattan heuristic	of a given board position
-def manhattan(board):
+#measures a heuristic of a given board position
+def heuristic(board, alt = False):
 	h = 0
 	for i in range(15):
 		col = 0
@@ -77,26 +79,42 @@ def manhattan(board):
 			if col == 4:
 				col = 0
 				row += 1
-		h += abs(loc[0] - row) + abs(loc[1] - col)
+		distance = abs(loc[0] - row) + abs(loc[1] - col)
+		h += distance
+	#linear conflict addition
+	if alt and distance > 0:
+		locRow = loc[0]
+		locCol = loc[1]
+		for j in range(4):
+			if loc[0] != j:
+				if board[j][locCol][0] != j * 4 + locCol:
+					if board[j][locCol][0] % locCol == 0:
+						h += 2
+		for k in range(4):
+			if loc[0] != k:
+				if board[locRow][0][k] != (k * 4 + locRow):
+					if board[locRow][0][k] % locRow == 0:
+						h += 2
 	return h
 
 #initiates an IDA* search
-def IDA(root):
-	bound = manhattan(root)
+def IDA(root, heu = False):
+	bound = heuristic(root)
 	path = [root]
 	t = ""
 	while t != "found":
 		#begins a recursive search
-		t = IDASearch(path, 0, bound)
+		t = IDASearch(path, 0, bound, heu, 0)
 		#increases the bound, allowing the search to go deeper
-		bound = t
 	return t
 
-#performs a recurseive search using a manhattan heuristic
-def IDASearch(path, g, bound):
+#performs a recurseive search using a heuristic heuristic
+def IDASearch(path, g, bound, heu, count):
+	print(count)
+	global globalA
+	globalA += 1
 	root = path[-1]
-	f = g + manhattan(root)
-
+	f = g + heuristic(root, heu)
 	#print(f)
 	if f > bound:
 		return f
@@ -120,31 +138,33 @@ def IDASearch(path, g, bound):
 
 			#print(root)
 			path.append(i)
-			t = IDASearch(path, g + 1, bound)
+			t = IDASearch(path, g + 1, bound, heu, count + 1)
 			if t == "found":
 				return "found"
 			elif t < min:
 				min = t
 			del path[-1]
+	
 	return min
 
 #performs a recursive best first search
-#3114
-def RBFS(board, fLimit):
-	fn = manhattan(board)
+
+def RBFS(board, fLimit, heu = False):
+	print(count)
+	global globalA
+	fn = heuristic(board, heu)
 	if goalTest(board):
-		return "success", manhattan(board)
+		return "success", heuristic(board, heu)
 	choice = []
-	for i in range(4):
+	for i in range(5):
 		if move(board, compass[i]) != []:
-			moveCost = manhattan((move(board, compass[i])))
+			moveCost = heuristic((move(board, compass[i])), heu)
 			choice.append((move(board, compass[i]), max(moveCost,fn)))
 
 	succ = [choice[0]]
 	for i in range(1, len(choice)):
-		#print(i)
 		for j in range(len(succ)):
-			if manhattan(choice[i][0]) < manhattan(succ[j][0]):
+			if (choice[i][1]) <= (succ[j][1]):
 				succ.insert(j,choice[i])
 			elif j == len(succ) - 1:
 				succ.append(choice[i])
@@ -152,20 +172,69 @@ def RBFS(board, fLimit):
 	control = True
 	while control == True:
 		#print(succ)
-		best = succ[0][0]
-		#print(manhattan(best))
+		#print("***")
+		best = []
+		best.append(succ[0][0])
+		best.append(succ[0][1])
+		#print((best))
 		alt = succ[1][0]
-		if manhattan(best) > fLimit:
-			return "Fail", manhattan(best)
-		result, best = RBFS(best, min(fLimit, manhattan(alt)))
+		if heuristic(best[0], heu) > fLimit:
+			return "Fail", best[1]
+		result, best[1] = RBFS(best[0], min(fLimit, heuristic(alt, heu)), count + 1)
 		if result == "success":
-			return result, (best)
+			return result, best[1]
 			control = False
 
-
-
-b1 = createBoard()
-b1 = scramble(b1, 20)
-b2 = b1
-#print(IDA(b1))
-print(RBFS(b2, 50))
+b1 = createBoard()			
+b1 = scramble(b1, (0 + 5) * 10)
+b2 = b1	
+globalA = 0
+print(RBFS(b1))
+print(globalA)
+'''
+f = open("runtimes.csv", "w")
+f.write("IDA-manhattan, IDA-my, RBFS-manhattan, RBFS-my\n")
+for i in range(2,3):
+	f.write("i = ")
+	f.write(str(i + 1))
+	f.write("\n")
+	for j in range(10):
+		#print(str(i))
+		#print(str(j))
+		b1 = createBoard()
+		b1 = scramble(b1, (i + 1) * 10)
+		b2 = b1
+		b3 = b1
+		b4 = b1
+		start = time.time()
+		print(globalA)
+		globalA = 0
+		print(IDA(b1))
+		print(globalA)
+		GlobalA = 0
+		print("XXX")
+		end = time.time()
+		f.write(str(end-start))
+		f.write(",")
+		start = time.time()
+		#print(IDA(b2, True))
+		#print("###")
+		end = time.time()
+		f.write(str(end-start))
+		f.write(",")
+		start = time.time()
+		RBFS(b3, math.inf)
+		end = time.time()
+		f.write(str(end-start))
+		f.write(",")
+		start = time.time()
+		RBFS(b4, math.inf, True)
+		end = time.time()
+		f.write(str(end-start))
+		f.write("\n")
+		
+start = time.time()
+print(RBFS(b2, math.inf, True))
+end = time.time()
+print(end - start)
+'''
---
+++ sudoku.py
@@ -0,0 +1,181 @@
+import numpy as np
+
+#reads file of sudoku puzzles and places them in arrays arranged by difficulty (file is specifically formatted prior to input)
+def readPuzzleFile(file):
+	easy = []
+	medium = []
+	hard = []
+	evil = []
+	
+	difficulty = {
+		"Easy": 1,
+		"Medium": 2,
+		"Hard": 3,
+		"Evil": 4
+	}
+	
+	
+	#for each configuration in the text file:
+	f = open(file, "r")
+	file = f.read().splitlines()
+	board = np.zeros((9,9))
+	control = False
+	i = 0
+	
+	for line in file:
+		#determine which difficulty the puzzle is
+		if len(line.split()) > 1:
+			#place the board into the correct array
+			if control == True:
+				if difficulty[d] == 1:
+					easy.append(board)
+				elif difficulty[d] == 2:
+					medium.append(board)
+				elif difficulty[d] == 3:
+					hard.append(board)
+				elif difficulty[d] == 4:
+					evil.append(board)
+			control = True
+			#throw out first string and read the second
+			i = 0
+			d = line.split()[1]
+			board = np.zeros((9,9))
+		#read the following nine lines in as a 9x9 array of ints (the sudoku board)
+		else:
+			for j in range(9):
+				board[i][j] = int(line[j])
+			i += 1
+			if i == 9:
+				i = 0
+	return easy, medium, hard, evil
+	
+def alldiff(board, row, col):
+	if board[row][col] != 0:
+		return [board[row][col]]
+		
+	if row < 3:
+		squareRow = 0
+	elif row < 6:
+		squareRow = 3
+	else:
+		squareRow = 6
+	if col < 3:
+		squareCol = 0
+	elif col < 6:
+		squareCol = 3
+	else:
+		squareCol = 6
+	
+	domain = [1,2,3,4,5,6,7,8,9]
+	for i in range(9):
+		if board[row][i] in domain and board[row][i] != 0 and i != col:
+			domain.remove(board[row][i])
+		if board[i][col] in domain and board[i][col] != 0 and i != row:
+			domain.remove(board[i][col])
+	for i in range(3):
+		for j in range(3):
+			if board[squareRow + i][squareCol + j] in domain and board[squareRow + i][squareCol + j] != 0:
+				domain.remove(board[squareRow + i][squareCol + j])
+	return domain
+			
+	
+def success(board):
+	#check that board is filled
+	for i in range(9):
+		for j in range(9):
+			if board[i][j] == 0:
+				return False
+	#check that board has 
+	for i in range(9):
+		if allDiffLine(board, "col", i) == False:
+			return False
+		if allDiffLine(board, "row", i) == False:
+			return False
+	for i in range(3):
+		for j in range(3):
+			if allDiffSquare(board, i, j) == False:
+				return False
+	return True
+
+class assignment:
+	domain = []
+	board = []
+	def __init__(self, board):
+		self.board = board
+		for i in range(9):
+			self.domain.append([[],[],[],[],[],[],[],[],[]])
+			for j in range(9):
+				self.domain[i][j] = alldiff(board, i, j)
+				#will automatically account for naked singles
+				if len(self.domain[i][j]) == 1:
+					self.board[i][j] = self.domain[i][j][0]
+	
+	def makeAssignment(self, row, col, assignment):
+		self.board[row][col] = assignment
+		
+	def updateDomain(self):
+		for i in range(9):
+			for j in range(9):
+				self.domain[i][j] = alldiff(self.board, i, j)
+				if len(self.domain[i][j]) == 1:
+					self.board[i][j] = self.domain[i][j][0]
+
+	def assignmentComplete(self):
+		for i in range(9):
+			for j in range(9):
+				if len(self.domain[i][j]) != 1:
+					return False
+		return True
+	
+	def hiddenSingle(self):
+		for i in range(9):
+			for j in range(9):
+			
+				if i < 3:
+					squareRow = 0
+				elif i < 6:
+					squareRow = 3
+				else:
+					squareRow = 6
+				if j < 3:
+					squareCol = 0
+				elif j < 6:
+					squareCol = 3
+				else:
+					squareCol = 6
+					
+				control = False
+				for d in self.domain[i][j]:
+					controlRow = True
+					controlCol = True
+					controlBox = True
+					for x in range(9):
+						if control == False:
+							if d in self.domain[i][x] and x != j:
+								controlRow = False
+							if d in self.domain[x][j] and x != i:
+								controlCol = False
+					for x in range(3):
+						for y in range(3):
+							if d in self.domain[squareCol + x][squareRow + y]:
+								controlBox = False
+								
+					if controlRow or controlCol or controlBox:
+						self.makeAssignment(i,j, d)
+		
+
+def backtrackSearch(assign, board, varRow, varCol, MCV = False):
+	step = 0
+	if assign.assignmentComplete():
+		return assignment
+	
+	for i in assign.domain[varRow][varCol]:
+		tempAssign = assign
+		tempAssign.makeAssignment(varRow, varCol, i)
+		tempAssign.updateDomain()
+		
+	
+easy, medium, hard, evil = readPuzzleFile("sudoku-problems.txt")
+assign = assignment(easy[0])
+assign.hiddenSingle()
+print(assign.board)
